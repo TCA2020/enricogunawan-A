36a8bf56130a09044b250dae3e6595fa92d1f3cc
EnricoGunawan18 <20J70008eg@edu.tca.ac.jp>
2020-09-01 13:32:22 +0900

    Game01

---
+++ ConsoleApp2/Class1.cs
@@ -0,0 +1,40 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp2
+{
+    public class chara
+    {
+        public int Damage = 8;
+        public int HP = 40;
+        public void Attack(chara enemy)
+        {
+            if (this.Damage < 0)
+            {
+                this.Damage = 1;
+            }
+            enemy.HP -= this.Damage;
+        }
+        public void Heal(chara chara)
+        {
+            this.HP += 5;
+            if (this.HP > 40)
+            {
+                this.HP = 40;
+            }
+        }
+
+        public void weaken(chara enemy)
+        {
+            enemy.Damage -= 1;
+        }
+
+        public void Strength(chara chara)
+        {
+            this.Damage += 3;
+        }
+    }
+
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,131 @@
+﻿
+using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            chara charaInstance = new chara();
+            chara enemyInstance = new chara();
+
+            Console.WriteLine("Q = 攻撃, W = 回復, E = 敵の攻撃力を減らす, R = 自分の攻撃力を強化");
+
+            while (charaInstance.HP > 0 && enemyInstance.HP > 0)
+            {
+                char atk = (char)Console.Read();
+
+                if (atk == 'Q' || atk == 'q')
+                {
+                    charaInstance.Attack(enemyInstance);
+                    Random enemyatk = new Random();
+                    if (enemyatk.Next(0, 3) == 0)
+                    {
+                        enemyInstance.Attack(charaInstance);
+                    }
+                    else if (enemyatk.Next(0, 3) == 1)
+                    {
+                        enemyInstance.Heal(enemyInstance);
+                    }
+                    else if (enemyatk.Next(0, 3) == 2)
+                    {
+                        enemyInstance.weaken(charaInstance);
+                    }
+                    else if (enemyatk.Next(0, 3) == 3)
+                    {
+                        enemyInstance.Strength(charaInstance);
+                    }
+
+                    Console.WriteLine("君のHP = " + charaInstance.HP + " 攻撃力 = " + charaInstance.Damage);
+                    Console.WriteLine("敵のHP = " + enemyInstance.HP + " 攻撃力 = " + enemyInstance.Damage + "\n");
+
+                }
+                else if (atk == 'W' || atk == 'w')
+                {
+                    charaInstance.Heal(charaInstance);
+                    Random enemyatk = new Random();
+                    if (enemyatk.Next(0, 3) == 0)
+                    {
+                        enemyInstance.Attack(charaInstance);
+                    }
+                    else if (enemyatk.Next(0, 3) == 1)
+                    {
+                        enemyInstance.Heal(enemyInstance);
+                    }
+                    else if (enemyatk.Next(0, 3) == 2)
+                    {
+                        enemyInstance.weaken(charaInstance);
+                    }
+                    else if (enemyatk.Next(0, 3) == 3)
+                    {
+                        enemyInstance.Strength(charaInstance);
+                    }
+
+                    Console.WriteLine("君のHP = " + charaInstance.HP + " 攻撃力 = " + charaInstance.Damage);
+                    Console.WriteLine("敵のHP = " + enemyInstance.HP + " 攻撃力 = " + enemyInstance.Damage + "\n");
+
+                }
+                else if (atk == 'E' || atk == 'e')
+                {
+                    charaInstance.weaken(enemyInstance);
+                    Random enemyatk = new Random();
+                    if (enemyatk.Next(0, 3) == 0)
+                    {
+                        enemyInstance.Attack(charaInstance);
+                    }
+                    else if (enemyatk.Next(0, 3) == 1)
+                    {
+                        enemyInstance.Heal(enemyInstance);
+                    }
+                    else if (enemyatk.Next(0, 3) == 2)
+                    {
+                        enemyInstance.weaken(charaInstance);
+                    }
+                    else if (enemyatk.Next(0, 3) == 3)
+                    {
+                        enemyInstance.Strength(charaInstance);
+                    }
+
+                    Console.WriteLine("君のHP = " + charaInstance.HP + " 攻撃力 = " + charaInstance.Damage);
+                    Console.WriteLine("敵のHP = " + enemyInstance.HP + " 攻撃力 = " + enemyInstance.Damage + "\n");
+
+                }
+                else if (atk == 'R' || atk == 'r')
+                {
+                    charaInstance.Strength(enemyInstance);
+                    Random enemyatk = new Random();
+                    if (enemyatk.Next(0, 3) == 0)
+                    {
+                        enemyInstance.Attack(charaInstance);
+                    }
+                    else if (enemyatk.Next(0, 3) == 1)
+                    {
+                        enemyInstance.Heal(enemyInstance);
+                    }
+                    else if (enemyatk.Next(0, 3) == 2)
+                    {
+                        enemyInstance.weaken(charaInstance);
+                    }
+                    else if (enemyatk.Next(0, 3) == 3)
+                    {
+                        enemyInstance.Strength(charaInstance);
+                    }
+
+                    Console.WriteLine("君のHP = " + charaInstance.HP + " 攻撃力 = " + charaInstance.Damage);
+                    Console.WriteLine("敵のHP = " + enemyInstance.HP + " 攻撃力 = " + enemyInstance.Damage + "\n");
+
+                }
+            }
+
+            if (charaInstance.HP <= 0)
+            {
+                Console.WriteLine("\n負けです");
+            }
+            else if (enemyInstance.HP <= 0)
+            {
+                Console.WriteLine("\n勝ちです");
+            }
+        }
+    }
+}
